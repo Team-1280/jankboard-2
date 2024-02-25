585a5b578beb2a45318347f34366f720e2d17e06
Youwen Wu <youwenw@gmail.com>
2024-02-24 20:21:42 -0800

    feat: add jsdoom

---
+++ client/src/lib/Apps/JSDoom/JSDoom.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import AppContainer from "../AppContainer.svelte";
+  import { doomBootupSequence } from "../../Sequences/sequences";
+  import { Notifications } from "../../Notifications/notifications";
+
+  onMount(() => {
+    doomBootupSequence();
+  });
+
+  const handleError = () => {
+    Notifications.error(
+      "Failed to load the Doom app. Did you add it to the app/static/external-apps directory?"
+    );
+  };
+</script>
+
+<AppContainer>
+  <iframe
+    title="JSDoom"
+    src="/static/external-apps/jsdoom/index.html"
+    class="w-full h-screen rounded-xl"
+    frameborder="0"
+    on:error={handleError}
+  />
+</AppContainer>

0d77ea7f84e25bb1342f4d0a151dd432f0d90753
Youwen Wu <youwenw@gmail.com>
2024-02-27 00:00:08 -0800

    feat: add dynamic voice setting

--- client/src/lib/Dashboard/Visualization/CameraControls/utils/cameraStore.ts
+++ client/src/lib/Dashboard/Visualization/CameraControls/utils/cameraStore.ts
@@ -1,8 +1,7 @@
-import type CameraControls from 'camera-controls'
-import { writable } from 'svelte/store'
-import Hornet from '../../models/Hornet.svelte'
-import type { Mesh, Object3DEventMap } from 'three'
-import type { Group } from 'three/examples/jsm/libs/tween.module.js'
+import type CameraControls from "camera-controls";
+import { writable } from "svelte/store";
+import type { Mesh, Object3DEventMap } from "three";
+import type { Group } from "three/examples/jsm/libs/tween.module.js";
 
-export const cameraControls = writable<CameraControls>()
-export const mesh = writable<Mesh>()
+export const cameraControls = writable<CameraControls>();
+export const mesh = writable<Mesh>();

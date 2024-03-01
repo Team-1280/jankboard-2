afc6dc01d06e733b1a054482f8304baba9c9d00d
Youwen Wu <youwenw@gmail.com>
2024-03-01 12:52:34 -0800

    style: remove commented out code and unused imports

--- client/src/lib/Dashboard/Visualization/Visualization.svelte
+++ client/src/lib/Dashboard/Visualization/Visualization.svelte
@@ -1,32 +1,6 @@
 <script lang="ts">
   import { Canvas } from '@threlte/core'
-  import { cameraControls } from './CameraControls/utils/cameraStore'
   import Scene from './Scene.svelte'
-  import { onMount } from 'svelte'
-  import { type OrthographicCamera, type PerspectiveCamera } from 'three'
-
-  let camera: PerspectiveCamera | OrthographicCamera
-
-  $: if ($cameraControls) {
-    // @ts-expect-error
-    camera = $cameraControls._camera
-  }
-
-  onMount(() => {
-    // setTimeout(() => {
-    //   // $cameraControls.moveTo(3, 5, 2, true)
-    //   // $cameraControls.setOrbitPoint(20, 20, 20)
-    //   $cameraControls.setLookAt(
-    //     3,
-    //     5,
-    //     2,
-    //     $mesh.position.x,
-    //     $mesh.position.y,
-    //     $mesh.position.z,
-    //     true
-    //   )
-    // }, 8000)
-  })
 </script>
 
 <Canvas>

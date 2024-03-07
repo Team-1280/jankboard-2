db2819d89d81cd41641e98b623d1a2aebcf3fa55
Youwen Wu <youwenw@gmail.com>
2024-03-06 23:55:35 -0800

    chore: remove unused functions and imports

--- client/src/lib/utils/initializeTelemetry.ts
+++ client/src/lib/utils/initializeTelemetry.ts
@@ -9,23 +9,8 @@ import { listen } from '@tauri-apps/api/event'
  * which will be called with the NetworkTable object every time an update is received from the backend.
  */
 
-const onUpdate = (data: TelemetryData) => {
-  telemetryStore.update(data)
-  // console.log(data)
-}
-
-export const initializeTelemetry = async (
-  topics: TelemetryTopics,
-  refreshRate: number
-) => {
-  // Make sure refreshRate is valid
-  if (!Number.isInteger(refreshRate) || refreshRate < 1) {
-    throw new Error(
-      'refreshRate must be an integer greater than or equal to 1.'
-    )
-  }
-
-  const unlistenStatus = await listen('telemetry_status', event => {
+export const initializeTelemetry = async () => {
+  const unlistenStatus = await listen('telemetry_status', (event) => {
     if (event.payload === 'connected') {
       telemetryStore.set('connected', true)
     } else if (event.payload === 'disconnected') {
@@ -33,7 +18,7 @@ export const initializeTelemetry = async (
     }
   })
 
-  const unlistenTelemetry = await listen('telemetry_data', event => {
+  const unlistenTelemetry = await listen('telemetry_data', (event) => {
     const data = JSON.parse(event.payload as string)
     telemetryStore.set(data['topic_name'], data['data'])
   })

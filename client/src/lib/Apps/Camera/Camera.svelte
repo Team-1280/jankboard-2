9a4713ea145b48d87b81e96297b2b2fe33292a79
Youwen Wu <youwenw@gmail.com>
2024-03-18 22:18:56 -0700

    style: increase gap between camera feeds

--- client/src/lib/Apps/Camera/Camera.svelte
+++ client/src/lib/Apps/Camera/Camera.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <AppContainer
-  class="px-10 py-20 flex gap-4 w-full backdrop-blur-lg justify-center h-full rounded-3xl shadow-md bg-slate-300 bg-opacity-30"
+  class="px-10 py-20 flex gap-8 w-full backdrop-blur-lg justify-center h-full rounded-3xl shadow-md bg-slate-300 bg-opacity-30"
 >
   <div class="my-auto">
     <CameraContainer cameraUrl={$settingsStore.frontCameraAddr} />

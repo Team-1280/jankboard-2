f9439a4f0f6a97db9084316535513ed2b57763ae
Youwen Wu <youwenw@gmail.com>
2024-03-18 21:56:25 -0700

    style: center camera cutouts

--- client/src/lib/Apps/Camera/Camera.svelte
+++ client/src/lib/Apps/Camera/Camera.svelte
@@ -7,10 +7,12 @@
 <AppContainer
   class="px-10 py-20 flex gap-4 w-full backdrop-blur-lg justify-center h-full rounded-3xl shadow-md bg-slate-300 bg-opacity-30"
 >
-  <div class="my-auto">
-    <CameraContainer cameraUrl={$settingsStore.frontCameraAddr} />
-  </div>
-  <div class="my-auto">
-    <CameraContainer cameraUrl={$settingsStore.rearCameraAddr} />
+  <div class="flex flex-col justify-center gap-2">
+    <div class="my-auto">
+      <CameraContainer cameraUrl={$settingsStore.frontCameraAddr} />
+    </div>
+    <div class="my-auto">
+      <CameraContainer cameraUrl={$settingsStore.rearCameraAddr} />
+    </div>
   </div>
 </AppContainer>
